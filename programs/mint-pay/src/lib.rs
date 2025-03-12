df0316a321eb9e3fea83a8451513b0a3d83350ad
Juiiceee <lbehr@student.42.fr>
2025-03-12 11:37:00 +0100

    il me manque un tout petit truc :(

--- programs/mint-pay/src/lib.rs
+++ programs/mint-pay/src/lib.rs
@@ -1,8 +1,11 @@
 use anchor_lang::prelude::*;
 
-use mpl_core::{instructions::CreateV1CpiBuilder, types::DataState};
+use mpl_core::{
+    instructions::{CreateCollectionV1CpiBuilder, CreateV1CpiBuilder},
+    types::DataState,
+};
 
-declare_id!("P4cYdsn1RYax3nA8Pmw54rpraBxKUmPU6PU1ZQC39jA");
+declare_id!("mqukZBPWH6K7JXX3V4VEJGJYFvEwpSMpT4y9DCweU3k");
 
 #[program]
 pub mod mint_pay {
@@ -11,14 +14,32 @@ pub mod mint_pay {
     pub fn initialize_mint(ctx: Context<MintAsset>, name: String, uri: String) -> Result<()> {
         CreateV1CpiBuilder::new(&ctx.accounts.mpl_core_program.to_account_info())
             .asset(&ctx.accounts.mint.to_account_info())
-            .collection(None)
+            .collection(Some(&ctx.accounts.collection.to_account_info()))
             .payer(&ctx.accounts.user.to_account_info())
-            .update_authority(None)
             .system_program(&ctx.accounts.system_program.to_account_info())
             .data_state(DataState::AccountState)
             .name(name)
             .uri(uri)
             .invoke()?;
+
+        Ok(())
+    }
+
+    pub fn initialize_collection(
+        ctx: Context<MintCollection>,
+        name: String,
+        uri: String,
+    ) -> Result<()> {
+        CreateCollectionV1CpiBuilder::new(&ctx.accounts.mpl_core_program.to_account_info())
+            .collection(&ctx.accounts.collection.to_account_info())
+            .payer(&ctx.accounts.user.to_account_info())
+            .system_program(&ctx.accounts.system_program.to_account_info())
+            .name(name)
+            .uri(uri)
+            .invoke()?;
+
+        // Stocker la clé publique de la collection dans notre compte
+        ctx.accounts.collection_account.owner = ctx.accounts.collection.key();
 
         Ok(())
     }
@@ -31,8 +52,30 @@ pub struct MintAsset<'info> {
     /// CHECK: This is the mint account of the asset to be minted
     #[account(mut)]
     pub mint: Signer<'info>,
+    /// CHECK: This is the collection account of the asset to be minted
+    #[account(mut)]
+    pub collection: AccountInfo<'info>,
     pub system_program: Program<'info, System>,
     /// CHECK: This is the ID of the Metaplex Core program
     #[account(address = mpl_core::ID)]
     pub mpl_core_program: UncheckedAccount<'info>,
+}
+
+#[derive(Accounts)]
+pub struct MintCollection<'info> {
+    #[account(init, payer = user, seeds = [b"collection", user.key().as_ref()], bump, space = 8 + 32)]
+    pub collection_account: Account<'info, Collection>,
+    #[account(mut)]
+    pub user: Signer<'info>,
+    #[account(mut)]
+    pub collection: Signer<'info>,
+    pub system_program: Program<'info, System>,
+    /// CHECK: This is the ID of the Metaplex Core program
+    #[account(address = mpl_core::ID)]
+    pub mpl_core_program: UncheckedAccount<'info>,
+}
+
+#[account]
+pub struct Collection {
+    pub owner: Pubkey,
 }
