7f2e80f373e7063f928f3af60a48a822edb232e8
Juiiceee <lbehr@student.42.fr>
2025-03-15 21:14:27 +0100

    ca fonctionne mais je dois changer des trucs

--- programs/mint-pay/src/lib.rs
+++ programs/mint-pay/src/lib.rs
@@ -1,7 +1,7 @@
 use anchor_lang::prelude::*;
 
-mod states;
 mod instructions;
+mod states;
 
 use instructions::*;
 
@@ -12,7 +12,9 @@ pub mod mint_pay {
     use super::*;
 
     pub fn initialize_mint(ctx: Context<MintAsset>) -> Result<()> {
-        ctx.accounts.initialize_mint()
+        // Access the admin bump directly from ctx.bumps
+        let admin_bump = ctx.bumps.admin;
+        ctx.accounts.initialize_mint(admin_bump)
     }
 
     pub fn initialize_collection(
@@ -23,6 +25,26 @@ pub mod mint_pay {
     ) -> Result<()> {
         // Access the admin bump directly from ctx.bumps
         let admin_bump = ctx.bumps.admin;
-        ctx.accounts.initialize_collection(name, uri, price, admin_bump)
+        ctx.accounts
+            .initialize_collection(name, uri, price, admin_bump)
     }
+
+    pub fn test_ids(ctx: Context<TestIds>) -> Result<()> {
+        msg!("System Program ID: {}", ctx.accounts.system_program.key());
+        msg!(
+            "MPL Core Program ID: {}",
+            ctx.accounts.mpl_core_program.key()
+        );
+        msg!("mpl_core::ID: {}", mpl_core::ID);
+        Ok(())
+    }
+}
+
+#[derive(Accounts)]
+pub struct TestIds<'info> {
+    #[account(mut)]
+    pub user: Signer<'info>,
+    pub system_program: Program<'info, System>,
+    /// CHECK: This is the ID of the Metaplex Core program
+    pub mpl_core_program: UncheckedAccount<'info>,
 }
